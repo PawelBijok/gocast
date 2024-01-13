91dbf0aa75deca2f2fdd011267e5e28852c017af
Paweł Bijok <bijok.pawel@gmail.com>
2024-01-13 09:51:58 +0100

    change prefs file location

--- internal/settings/settings_service.go
+++ internal/settings/settings_service.go
@@ -9,14 +9,28 @@ import (
 	e "github.com/pafello/gocast/internal/errors"
 )
 
-const settingsFilePath = "./go_cast_settings.json"
+const settingsFilePath = "/go_cast_settings.json"
+
+func getConfigFile() (string, error) {
+	dirname, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+	return dirname + "/.config" + settingsFilePath, nil
+}
 
 func SaveUserSettings(settings UserSettings) error {
 	bytes, err := json.Marshal(settings)
 	if err != nil {
 		return err
 	}
-	err = os.WriteFile(settingsFilePath, bytes, 0666)
+	filePath, err := getConfigFile()
+	if err != nil {
+
+		fmt.Println("Could not get config file")
+		return err
+	}
+	err = os.WriteFile(filePath, bytes, 0666)
 	if err != nil {
 		fmt.Println("Could not write to file")
 		return err
@@ -26,7 +40,13 @@ func SaveUserSettings(settings UserSettings) error {
 }
 func GetUserSettings() (UserSettings, error) {
 
-	fileBytes, err := os.ReadFile(settingsFilePath)
+	filePath, err := getConfigFile()
+	if err != nil {
+
+		fmt.Println("Could not get config file")
+		return UserSettings{}, err
+	}
+	fileBytes, err := os.ReadFile(filePath)
 	if err != nil {
 		fmt.Println("Could not get file")
 		return UserSettings{}, errors.New(e.FileDoesNotExists)
